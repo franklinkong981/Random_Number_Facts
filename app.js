c48ceec21423234148600e6cd9c0493f2be68a5b
Franklin Kong <franklinkong981@gmail.com>
2024-07-17 15:04:30 -0700

    Used async/await instead of Promises

--- app.js
+++ app.js
@@ -2,29 +2,28 @@ const getFactsButton = document.querySelector('.get-facts');
 const numberReveal = document.querySelector('.number-reveal');
 const factsList = document.querySelector('.facts-list');
 
-getFactsButton.addEventListener('click', function() {
+getFactsButton.addEventListener('click', async function() {
     let fourRandomFacts = [];
     let url = 'http://numbersapi.com/random/trivia?json';
     let favoriteNumber = 0;
 
-    //get the number the facts will be on.
-    axios.get(url)
-    .then(res => {
-		favoriteNumber = res.data.number;
+    try {
+        //get the number the facts will be on.
+        let res = await axios.get(url);
+        favoriteNumber = res.data.number;
         numberReveal.innerText = `The number is: ${favoriteNumber}`;
         //generate the four facts about that number.
         for (let i = 1; i < 5; i++) {
             fourRandomFacts.push(
-                axios.get(`http://numbersapi.com/${favoriteNumber}/trivia?json`)
+                await axios.get(`http://numbersapi.com/${favoriteNumber}/trivia?json`)
             );
         }
-        return Promise.all(fourRandomFacts)
-	})
-    .then(randomFactsArray => {
         for (let i = 0; i < 4; i++) {
-            factsList.children[i].innerText = randomFactsArray[i].data.text;
+            factsList.children[i].innerText = fourRandomFacts[i].data.text;
         }
-    })
-    .catch(err => console.log("REJECTED!", err));
+    }
+    catch(e) {
+        console.log("REJECTED!");
+    }
 });
 
